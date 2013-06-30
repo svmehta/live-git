408d8e14d838ff2caece68b780d254fdf4c4e002
Hourann Bosci <hourannb@dview.net>
2013-06-30 02:34:20 -0700

    remove old testClient code, slight cleanup of client.js
    not sure if this breaks meteor magic?

--- meteor-live-git/client/client.js
+++ meteor-live-git/client/client.js
@@ -1,24 +1,39 @@
 // Main template
-Template.main.repository = function() {
-    console.log(Repositories.findOne());  // TODO remove
-    return Repositories.findOne(); 
-}
+var getRepository = function() {
+  return Repositories.findOne();
+};
 
-Template.main.users = function() {
-    var users = Users.find();   // TODO filter for current repository
+var getUsers = function() {
+  var users = Users.find();   // TODO filter for current repository
 
-    var usersWithWorkingCopy = _.map(users, function(user) {
-        var workingCopy = WorkingCopies.findOne({ userId: user._id, }, 
-            { sort:  { timestamp: -1}}); 
-        console.log(workingCopies)
-        return {
-            "user": user,
-            "workingCopy": workingCopy
-        };
-    });
+  var usersWithWorkingCopy = _.map(users, function(user) {
+    var workingCopy = WorkingCopies.findOne({ userId: user._id, },
+      { sort:  { timestamp: -1 }});
+    console.log(workingCopy)
+    return {
+      "user": user,
+      "workingCopy": workingCopy
+    };
+  });
 
-    return usersWithWorkingCopy;
-}
-
+  return usersWithWorkingCopy;
+};
 
 
+Template.main.repository = getRepository();
+
+Template.main.users = getUsers();
+
+
+
+// Template.main.repository = { name: "My Awesome Repo" }
+//
+// Template.main.users = [
+//   {
+//     name: "img/hourann.jpg", email: "hourannb@dview.net"
+//   }
+// ];
+//
+// Template.user.gravatar = function() {
+//   return this.name;
+// }
